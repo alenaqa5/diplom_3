dfc6d5ee798097b4b817601c3b512e01d39d9634
AlenaQa5 <alyonagraun@gmail.com>
2024-10-04 08:02:20 +0200

    Изменены методы с использованием ожиданий. Параметр с ожиданиями вынесен как атрибут объекта класса

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,19 +5,20 @@ from selenium.webdriver.support import expected_conditions
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
+        self.time = 10
 
     def wait_and_find_element(self, locator) -> WebElement:
-        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(locator))
+        WebDriverWait(self.driver, self.time).until(expected_conditions.visibility_of_element_located(locator))
         return self.driver.find_element(*locator)
 
     def wait_element_to_be_clickable(self, locator) -> WebElement:
-        WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(locator))
+        WebDriverWait(self.driver, self.time).until(expected_conditions.element_to_be_clickable(locator))
         return self.driver.find_element(*locator)
 
 
     def is_element_visible(self, locator):
         try:
-            element = WebDriverWait(self.driver, 5).until(
+            element = WebDriverWait(self.driver, self.time).until(
                 expected_conditions.visibility_of_element_located(locator)
             )
             return True
